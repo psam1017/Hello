d0940c60477c43fb786edd44320fdd07de7b188f
psam1017 <psam1017@naver.com>
2023-06-20 10:26:36 +0900

    finished CH.4 mustache

--- src/main/java/com/example/Hello/service/posts/PostsService.java
+++ src/main/java/com/example/Hello/service/posts/PostsService.java
@@ -2,12 +2,17 @@ package com.example.Hello.service.posts;
 
 import com.example.Hello.domain.posts.Posts;
 import com.example.Hello.domain.posts.PostsRepository;
+import com.example.Hello.web.dto.PostsListResponseDto;
 import com.example.Hello.web.dto.PostsResponseDto;
 import com.example.Hello.web.dto.PostsSaveRequestDto;
 import com.example.Hello.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -30,6 +35,21 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAll(Sort.by("id").descending()).stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        postsRepository.delete(posts);
     }
 
     public PostsResponseDto findById(Long id) {
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,6 +4,12 @@ var main = {
         $('#btn-save').on('click', function () {
             _this.save();
         });
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
     },
     save: function () {
         var data = {
@@ -19,11 +25,47 @@ var main = {
             contentType: 'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function () {
-            alert('글이 등록되었습니다.');
+            alert('The post is saved!');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
+    },
+    update: function () {
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: `/api/v1/posts/${id}`,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('The Post is updated!');
+            window.location.href="/";
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        })
+    },
+    delete: function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: `/api/v1/posts/${id}`,
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+        }).done(function () {
+            alert('The Post is deleted!');
+            window.location.href="/";
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        })
     }
 };
 
